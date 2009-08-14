1b7dcd63fee447672263d8df1eea1cc2c6e241d0
Michael Ryan <mryan3@apache.org>
2009-08-14 18:18:28 +0000

    This deals with a strange behavior I found in nsupdate -- it sometimes hangs instead of exiting after perfoming a DNS update.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tashi/trunk@804316 13f79535-47bb-0310-9956-ffa450edef68

--- src/tashi/agents/dhcpdns.py
+++ src/tashi/agents/dhcpdns.py
@@ -17,7 +17,10 @@
 
 import logging
 import os
+import signal
 import socket
+import subprocess
+import time
 from instancehook import InstanceHook
 from tashi.services.ttypes import Instance, NetworkConfiguration
 from tashi import boolean
@@ -148,39 +151,57 @@ class DhcpDns(InstanceHook):
 			cmd = "nsupdate -k %s" % (self.dnsKeyFile)
 		else:
 			cmd = "nsupdate"
-		(stdin, stdout) = os.popen2(cmd)
-		stdin.write("server %s\n" % (self.dnsServer))
-		stdin.write("update add %s.%s %d A %s\n" % (name, self.dnsDomain, self.dnsExpire, ip))
-		stdin.write("\n")
-		if (self.reverseDns):
-			ipSegments = map(int, ip.split("."))
-			ipSegments.reverse()
-			reverseIpStr = ("%d.%d.%d.%d.in-addr.arpa" % (ipSegments[0], ipSegments[1], ipSegments[2], ipSegments[3]))
-			stdin.write("update add %s %d IN PTR %s.%s.\n" % (reverseIpStr, self.dnsExpire, name, self.dnsDomain))
+		child = subprocess.Popen(args=cmd.split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+		try:
+			(stdin, stdout) = (child.stdin, child.stdout)
+			stdin.write("server %s\n" % (self.dnsServer))
+			stdin.write("update add %s.%s %d A %s\n" % (name, self.dnsDomain, self.dnsExpire, ip))
 			stdin.write("\n")
-		stdin.close()
-		output = stdout.read()
-		stdout.close()
-
+			if (self.reverseDns):
+				ipSegments = map(int, ip.split("."))
+				ipSegments.reverse()
+				reverseIpStr = ("%d.%d.%d.%d.in-addr.arpa" % (ipSegments[0], ipSegments[1], ipSegments[2], ipSegments[3]))
+				stdin.write("update add %s %d IN PTR %s.%s.\n" % (reverseIpStr, self.dnsExpire, name, self.dnsDomain))
+				stdin.write("\n")
+			stdin.close()
+			output = stdout.read()
+			stdout.close()
+		finally:
+			os.kill(child.pid, signal.SIGTERM)
+			(pid, status) = os.waitpid(child.pid, os.WNOHANG)
+			while (pid == 0): 
+				time.sleep(0.5)
+				os.kill(child.pid, signal.SIGTERM)
+				(pid, status) = os.waitpid(child.pid, os.WNOHANG)
+	
 	def removeDns(self, name):
 		if (self.dnsKeyFile != ""):
 			cmd = "nsupdate -k %s" % (self.dnsKeyFile)
 		else:
 			cmd = "nsupdate"
-		(stdin, stdout) = os.popen2(cmd)
-		stdin.write("server %s\n" % (self.dnsServer))
-		if (self.reverseDns):
-			ip = socket.gethostbyname(name)
-			ipSegments = map(int, ip.split("."))
-			ipSegments.reverse()
-			reverseIpStr = ("%d.%d.%d.%d.in-addr.arpa" % (ipSegments[0], ipSegments[1], ipSegments[2], ipSegments[3]))
-			stdin.write("update delete %s IN PTR\n" % (reverseIpStr))
+		child = subprocess.Popen(args=cmd.split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+		try:
+			(stdin, stdout) = (child.stdin, child.stdout)
+			stdin.write("server %s\n" % (self.dnsServer))
+			if (self.reverseDns):
+				ip = socket.gethostbyname(name)
+				ipSegments = map(int, ip.split("."))
+				ipSegments.reverse()
+				reverseIpStr = ("%d.%d.%d.%d.in-addr.arpa" % (ipSegments[0], ipSegments[1], ipSegments[2], ipSegments[3]))
+				stdin.write("update delete %s IN PTR\n" % (reverseIpStr))
+				stdin.write("\n")
+			stdin.write("update delete %s.%s A\n" % (name, self.dnsDomain))
 			stdin.write("\n")
-		stdin.write("update delete %s.%s A\n" % (name, self.dnsDomain))
-		stdin.write("\n")
-		stdin.close()
-		output = stdout.read()
-		stdout.close()
+			stdin.close()
+			output = stdout.read()
+			stdout.close()
+		finally:
+			os.kill(child.pid, signal.SIGTERM)
+			(pid, status) = os.waitpid(child.pid, os.WNOHANG)
+			while (pid == 0): 
+				time.sleep(0.5)
+				os.kill(child.pid, signal.SIGTERM)
+				(pid, status) = os.waitpid(child.pid, os.WNOHANG)
 	
 	def doUpdate(self, instance):
 		newInstance = Instance()
