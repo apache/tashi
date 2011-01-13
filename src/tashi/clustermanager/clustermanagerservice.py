22b1f09267a7ddbc29ca0fe68ee3774f0e7422b2
Michael Stroucken <stroucki@apache.org>
2011-01-13 00:09:33 +0000

    clustermanagerservice: add extra comment
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tashi/trunk@1058369 13f79535-47bb-0310-9956-ffa450edef68

--- src/tashi/clustermanager/clustermanagerservice.py
+++ src/tashi/clustermanager/clustermanagerservice.py
@@ -1,4 +1,4 @@
-# Licensed to the Apache Software Foundation (ASF) under one
+ # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
 # regarding copyright ownership.  The ASF licenses this file
@@ -214,6 +214,8 @@ class ClusterManagerService(object):
 			self.stateTransition(instance, InstanceState.Activating, InstanceState.Destroying)
 			self.data.releaseInstance(instance)
 		else:
+			# XXXstroucki: This is a problem with keeping
+			# clean state.
 			self.stateTransition(instance, None, InstanceState.Destroying)
 			self.data.releaseInstance(instance)
 
