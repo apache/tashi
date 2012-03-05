a0b6e8a53851745458bb15f31504e9e2194bb6a0
Michael Stroucken <stroucki@apache.org>
2012-03-05 18:29:01 +0000

    primitive: let primitive notice shutdowns too. The code for this should be rewritten.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tashi/trunk@1297148 13f79535-47bb-0310-9956-ffa450edef68

--- src/tashi/agents/primitive.py
+++ src/tashi/agents/primitive.py
@@ -251,7 +251,7 @@ class Primitive(object):
 				for i in oldInstances:
 					# XXXstroucki what about paused and saved VMs?
 					# XXXstroucki: do we need to look at Held VMs here?
-					if (i not in self.instances and (oldInstances[i].state == InstanceState.Running or oldInstances[i].state == InstanceState.Destroying)):
+					if (i not in self.instances and (oldInstances[i].state == InstanceState.Running or oldInstances[i].state == InstanceState.Destroying or oldInstances[i].state == InstanceState.ShuttingDown)):
 						self.log.info("VM exited: %s" % (oldInstances[i].name))
 						for hook in self.hooks:
 							hook.postDestroy(oldInstances[i])
