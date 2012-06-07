dec855d1ab0ab29a1f6a39293d8d2bcd225d39b0
Richard Gass <rgass@apache.org>
2012-06-07 13:17:54 +0000

    Only remove the logical volumn that the virtual machine created instead of blasting the entire volumn group.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tashi/trunk@1347602 13f79535-47bb-0310-9956-ffa450edef68

--- src/tashi/nodemanager/vmcontrol/qemu.py
+++ src/tashi/nodemanager/vmcontrol/qemu.py
@@ -220,8 +220,9 @@ class Qemu(VmControlInterface):
 				# remove scratch storage
 				try:
 					if self.scratchVg is not None:
-						log.info("Removing any scratch for %s" % (name))
-						cmd = "/sbin/lvremove --quiet -f %s" % self.scratchVg
+						scratchName = "lv%s" % name
+						log.info("Removing any scratch for %s" % (scratchName))
+						cmd = "/sbin/lvremove --quiet -f %s/%s" % (self.scratchVg, scratchName)
 						result = subprocess.Popen(cmd.split(), executable=cmd.split()[0], stdout=subprocess.PIPE, stderr=open(os.devnull, "w"), close_fds=True).wait()
 				except:
 					log.warning("Problem cleaning scratch volumes")
