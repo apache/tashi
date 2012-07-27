377c325a65a2c1735aa669f66fbce7403c3302c4
Michael Stroucken <stroucki@apache.org>
2012-07-27 04:17:37 +0000

    test: more learning on optparse
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tashi/trunk@1366274 13f79535-47bb-0310-9956-ffa450edef68

--- src/tashi/client/test.py
+++ src/tashi/client/test.py
@@ -3,13 +3,40 @@
 import optparse
 import sys
 
+def remoteCommand(command, options):
+	print "Doing command %s options %s" % (command, options)
+	return 0
+
 def setHostState(args):
 	parser = optparse.OptionParser()
-	parser.add_option("--host")
+	parser.set_usage("%s setHostState [options]" % sys.argv[0])
+	parser.add_option("--host", help="Set the state of this host", action="store", type="string", dest="hostname")
+	parser.add_option("--state", help="Change state to this value", action="store", type="string", dest="state")
+	(options, arguments) = parser.parse_args(args)
+	print options
+	print arguments
+	if options.hostname is None or options.state is None:
+		print "A mandatory option is missing\n"
+		parser.print_help()
+		sys.exit(-1)
+
+	remoteCommand("shs", options)
 	return 0
 
 def setHostNotes(args):
 	parser = optparse.OptionParser()
+	parser.set_usage("%s setHostNotes [options]" % sys.argv[0])
+	parser.add_option("--host", help="Annotate this host with the note (mandatory)", action="store", type="string", dest="hostname")
+	parser.add_option("--notes", help="Annotate the host with this note (mandatory)", action="store", type="string", dest="notes")
+	(options, arguments) = parser.parse_args(args)
+	print options
+	print arguments
+	if options.hostname is None or options.notes is None:
+		print "A mandatory option is missing\n"
+		parser.print_help()
+		sys.exit(-1)
+
+	remoteCommand("shn", options)
 	return 0
 
 cmdsdesc = (
